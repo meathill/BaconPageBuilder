508b5cda2d97368500ab9d7cf912762fa3ababa5
Meathill <meathill@gmail.com>
2012-02-20 09:12:44 +0800

    daily
    Signed-off-by: Meathill <meathill@gmail.com>

--- js/BannerMaker.js
+++ js/BannerMaker.js
@@ -5,7 +5,7 @@
  */
 jQuery.namespace('com.meathill.bacon');
 com.meathill.bacon.BannerModel = Backbone.Model.extend({
-  default: {
+  defaults: {
     "headPicUrl": '', // 大头图片url
     "callback": null,
     "saveURL": '',
@@ -14,25 +14,19 @@ com.meathill.bacon.BannerModel = Backbone.Model.extend({
     "body": "header"
   }
 });
-com.meathill.bacon.BannerMaker = function () {
-  var headPicUrl = ''; // 大头图片url
-  var callback = null;
-  var saveURL = '';
-  var styleIndex = -1;
-  var domID = "banner-maker";
-  var body = "header";
-  /**
-   * @constructor
-   */
-  var self = this;
-  /**
-   * Public Methods 
-   */
-  this.install = function (so, domid) {
-    domid = domid || domID;
+com.meathill.bacon.BannerMaker = Backbone.View.extend({
+  model: new com.meathill.bacon.BannerModel(),
+  tagName: 'div',
+  initialize: function () {
+    
+  },
+  render: function () {
+    
+  },
+  install: function () {
     var param = {
       allowScriptAccess: 'always',
-      wmode:'window'
+      wmode: 'window'
     };
     var flashvars = {
       
@@ -42,31 +36,21 @@ com.meathill.bacon.BannerMaker = function () {
     var src = "BannerProducer.swf";
     var express = "swf/expressInstall.swf";
     var version = '11';
-    so.embedSWF(src, domid, width, height, version, express, flashvars, param);
-    $('#' + body).height(height);
-  }
-  /**
-   * 将大头图片替换在页面代码内
-   * @param {Object} id 大头图片id
-   * @private
-   */
-  this.setHeadPic = function (url) {
-    headPicUrl = url;
-    if (this.callback != null) {
-      callback();
+    swfobject.embedSWF(src, this.model.get('domID'), width, height, version, express, flashvars, param);
+    this.setHeight(height);
+  },
+  setHeadPic: function (src) {
+    this.model.set('headPicUrl', url);
+    if (this.get('callback') != null) {
+      this.get('callback')();
     }
-  }
-  /**
-   * 切换模板后改变大头尺寸
-   * @param {Object} height
-   * @private
-   */
-  this.setHeight = function (height){
-    $('#' + domID).height(height);
-    $('#' + body).height(height);
-  }
-  this.setStyle = function (index) {
-    styleIndex = index;
+  },
+  setHeight: function (height) {
+    this.$('#' + this.model.get('domID')).height(height);
+    this.$('#' + this.model.get('body')).height(height);
+  },
+  setStyle: function (index) {
+    this.model.set('styleIndex', index);
     console.log('change css to : ', index);
   }
-}
+});
--- js/Main.js
+++ js/Main.js
@@ -8,7 +8,4 @@ $(function (evt) {
   // 调整功能面板大小
   GUI.onResize();
   $(window).resize(GUI.onResize);
-  // 调用方法构建模板
-  GUI.addressChangeHandler();
-  SWFAddress.addEventListener(SWFAddressEvent.CHANGE, GUI.addressChangeHandler);
 });
